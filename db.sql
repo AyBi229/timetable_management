1e8d430c7f5f59d0ea2a36e2cfaa3fd75419456f
Aya Birouti <aybi@klazomaniac-nimbostratus.>
2025-05-21 11:05:50 +0100

    i have realized that i don't need error handling since the user is logging in with existing credentials. so i did some changes

--- db.sql
+++ db.sql
@@ -1,1 +1,4 @@
 insert into users(id, cin, first_name, last_name, photo, birthday, email, `password`) values(null, 'aaa000', 'Aya', 'Birouti', null, '2004-09-02', 'biroutiaya@gmail.com', 'aybiisamazing@123');
+update users
+set password = 'AyBiisamazing@123'
+where id = 1;
--- resources/js/Pages/Login.jsx
+++ resources/js/Pages/Login.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Login.css';
 import { 
-  Autocomplete,
   Box, 
   Button,
   FormControl,
@@ -15,33 +14,12 @@ import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import { useQuery } from '@tanstack/react-query';
 import { fetchCountries } from '../Utilities/apis.js';
+import axios from 'axios';
 
 const Login = () => {
-  // use query hook
-  const { data: countries, isLoading, isError } = useQuery({
-    queryKey: ['countries'], // key
-    queryFn: fetchCountries // function
-  });
-
   // states
-  const [errors, setErrors] = React.useState({}); // error object
+  const [error, setError] = React.useState(null);
   const [showPassword, setShowPassword] = React.useState(false);
-  
-  /**
-   * DO NOT DELETE:
-   * fixing the issue concerning the disruption of the custom styling for the country selection autocomplete component 
-   */
-
-  // ref of country autocomplete component
-  const countrySearchInputRef = React.useRef(null);
-
-  // mounting execution only once
-  React.useEffect(() => {
-    // Manually focus the input field until after component has been rendered
-    if (countrySearchInputRef.current) {
-      countrySearchInputRef.current.focus(); // 
-    }
-  }, []); // <- the dependency array stops the component from getting focused multiple times on re-renders
 
   // handlers
   const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -61,36 +39,7 @@ const Login = () => {
     // Data
     const email = formData.get('email');
     const password = formData.get('password');
-  
-    // Clear previous errors
-    setErrors({});
-  
-    // Initialize a flag to track validation
-    let isValid = true;
-  
-    // Validate organization email
-    // regex for email validation
-    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailRegex.test(email)) {
-      setErrors((prevErrors) => ({ ...prevErrors, email: 'Invalid email address' }));
-      e.target.email.focus();
-      isValid = false;
-    }
-  
-    // Validate school name
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
-
-    if (!passwordRegex.test(password)) {
-      setErrors((prevErrors) => ({ ...prevErrors, password: 'Weak password' }));
-      e.target.password.focus();
-      isValid = false;
-    }
-  
-    // If valid, log the form values
-    if (isValid) {
-      const formValues = { email, password };
-      console.log(formValues);
-    }
+    
   }
   
   return (
@@ -100,7 +49,8 @@ const Login = () => {
         {/* title */}
         <div className='login-title-container'>
           <h1 className='login-title text-2xl font-bold'>Welcome back!</h1> {/* title */}
-          <em className='login-subtitle text-center'>Log in as the superuser</em> {/* subtitle */}
+          <em className='login-subtitle text-center'>Log in as superuser</em> {/* subtitle */}
+          { error && <small className='text-red-500'>One of the credentials are incorrect</small> }
         </div>
 
         {/* form */}
@@ -110,9 +60,7 @@ const Login = () => {
         >
           <TextField 
               id="email" 
-              error={ errors.email ? true : false }
-              helperText={ errors.email ? errors.email : '' }
-              label={ errors.email ? "Error" : "Email" } 
+              label="Email"
               variant="outlined"
               required
               name='email'
@@ -127,31 +75,41 @@ const Login = () => {
                 },
               }}
             />
-            <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="password"
-            type={showPassword ? 'text' : 'password'}
-            name='password'
-            error={errors.password} // --> check useFormControl in https://mui.com/material-ui/react-text-field/ <--
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label={
-                    showPassword ? 'hide the password' : 'display the password'
-                  }
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  onMouseUp={handleMouseUpPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-        </FormControl>
+            
+            <FormControl sx={{ width: '100%' }} variant="outlined">
+              <InputLabel htmlFor="password">Password</InputLabel>
+              <OutlinedInput
+                id="password"
+                required
+                type={showPassword ? 'text' : 'password'}
+                name='password'
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label={
+                        showPassword ? 'hide the password' : 'display the password'
+                      }
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      onMouseUp={handleMouseUpPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+                sx={{ 
+                  width: '100%', 
+                  '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                    borderColor: 'rgb(99, 99, 135)',  // Change border color on focus
+                  },
+                  '& .MuiInputLabel-root.Mui-focused': {
+                    color: 'rgb(99, 99, 135)',  // Change label color on focus
+                  },
+                }}
+              />
+            </FormControl>
             <Box>
               {/* verify submit button */}
               <Button 
